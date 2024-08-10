d438e51a70a464859a0dc86088df95402056f30e
Gozde <gozde.a.997@gmail.com>
2024-08-10 19:16:27 +0300

    asset changes,basic theme settings, textfield delegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var searchController: UITextField!
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
 
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func searchPressed(_ sender: Any) {
+        print("oy")
+        searchController.endEditing(true)
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchController.delegate = self // to access textfielddelegate
+    }
 
+    // cannot access this method withot uitextfielddelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(textField.text ?? "")
+        searchController.endEditing(true) // to close the keyboard
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchController.text?.removeAll()
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            return false
+        } else {
+            return true
+        }
+    }
 }
-
